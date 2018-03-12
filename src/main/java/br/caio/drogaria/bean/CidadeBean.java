0f1edd51515dbf7403d8d6b3ef8a09270470e1ef
Caio Maia <caio.maia03@gmail.com>
2018-03-12 15:24:32 -0300

    Salvar cidade concluido
    - inclusão do conversor Omni faces no generic domain

--- src/main/java/br/caio/drogaria/bean/CidadeBean.java
+++ src/main/java/br/caio/drogaria/bean/CidadeBean.java
@@ -10,29 +10,79 @@ import javax.faces.bean.ViewScoped;
 import org.omnifaces.util.Messages;
 
 import br.caio.drogaria.dao.CidadeDAO;
+import br.caio.drogaria.dao.EstadoDAO;
 import br.caio.drogaria.domain.Cidade;
+import br.caio.drogaria.domain.Estado;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class CidadeBean implements Serializable{
+public class CidadeBean implements Serializable {
+	private Cidade cidade;
 	private List<Cidade> cidades;
-	
+	private List<Estado> estados;
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+
 	public List<Cidade> getCidades() {
 		return cidades;
 	}
-	
+
 	public void setCidades(List<Cidade> cidades) {
 		this.cidades = cidades;
 	}
-	
+
 	@PostConstruct
-	public void listar(){
+	public void listar() {
 		try {
 			CidadeDAO cidadeDAO = new CidadeDAO();
 			cidades = cidadeDAO.listar();
-		} catch (RuntimeException erro){
+		} catch (RuntimeException erro) {
 			Messages.addFlashGlobalError("Ocorreu um erro ao tentar listar as cidades");
+			erro.printStackTrace();
+		}
+	}
+
+	public void novo() {
+		try {
+			cidade = new Cidade();
+
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar gerar uma nova cidade");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void salvar(){
+		try {
+			CidadeDAO cidadeDAO = new CidadeDAO();
+			cidadeDAO.merge(cidade);
+			
+			cidade = new Cidade();
+			cidades = cidadeDAO.listar();
+			
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+			
+			Messages.addGlobalInfo("Cidade salva com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar salvar uma nova cidade");
 			erro.printStackTrace();
 		}
 	}
